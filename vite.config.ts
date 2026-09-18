b48481474f1e94761dee78adcb875313b2ee7790
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2026-09-17 21:44:12 -0500

    add in setup for shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.config.ts'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     crx({ manifest }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
